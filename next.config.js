c8120b592b96753dc7bd86e7bd0df1eae73d8d7f
Victor Mota <vimota@gmail.com>
2020-03-28 22:35:54 -0700

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 const withMDX = require('@next/mdx')({
   extension: /[/\\](content)[/\\](.+)\.(md|mdx)?$/,
-})
-const withSass = require('@zeit/next-sass')
+});
 
-module.exports = withMDX(
-  withSass({
-    webpack(config, options) {
+module.exports = withMDX({
+    webpack(config, _) {
       // Further custom configuration here
       return config
     },
-  })
+  }
 )

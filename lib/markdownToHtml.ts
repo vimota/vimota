7d3548391dac3451edc5460c6e05657927e0757e
Victor Mota <vimota@gmail.com>
2023-10-29 15:46:54 -0700

    fix

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,16 +1,32 @@
-import unified from 'unified';
+import { unified } from 'unified'
 import markdown from 'remark-parse';
 import remarkToRehype from 'remark-rehype';
 import raw from 'rehype-raw';
 import sanitize from 'rehype-sanitize';
 import prism from '@mapbox/rehype-prism';
 import html from 'rehype-stringify';
+import { visit } from 'unist-util-visit';
+import remarkRehype from 'remark-rehype';
+
+const removeParagraphsInListItems = () => {
+  return (tree) => {
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'li' && node.children.length === 3 && node.children[1].tagName === 'p') {
+        node.children = node.children[1].children;
+      }
+    });
+  };
+};
+
+
+
 
 // Taken from https://github.com/zeit/next-site/blob/master/lib/docs/markdown-to-html.js.
 // Create the processor, the order of the plugins is important
 const getProcessor = unified()
   .use(markdown)
-  .use(remarkToRehype, { allowDangerousHtml: true })
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(removeParagraphsInListItems)
   // Add custom HTML found in the markdown file to the AST
   .use(raw)
   // Sanitize the HTML
@@ -26,7 +42,7 @@ export default async function markdownToHtml(md: string) {
     const file = await processor.process(md);
 
     // Replace non-breaking spaces (char code 160) with normal spaces to avoid style issues
-    return file.contents.toString().replace(/\xA0/g, ' ');
+    return file.toString().replace(/\xA0/g, ' ');
   } catch (error) {
     console.error(`Markdown to HTML error: ${error}`);
     throw error;
